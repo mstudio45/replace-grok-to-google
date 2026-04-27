80e243b5f361549034745c1386b0ba1aab0ffbb0
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2026-04-27 18:29:43 +0200

    v1.0.3: better config saving, create missing translate btn

--- main.user.js
+++ main.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Grok to Google Translation
-// @version      v1.0.2
+// @version      v1.0.3
 // @author       mstudio45
 // @description  Replaces Grok's translation with Google Translate.
 // @icon         https://abs.twimg.com/favicons/twitter.3.ico
@@ -8,20 +8,60 @@
 // @match        *://twitter.com/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_registerMenuCommand
 // @connect      translate.googleapis.com
 // ==/UserScript==
 
 (function() {
     const CONFIG = {
-        lang_to: "en",
-        debug: false
+        debug: GM_getValue("debug", false),
+
+        translateTo: GM_getValue("translateTo", "en"),
+        createMissingTranslateBtn: GM_getValue("createMissingTranslateBtn", false)
+    };
+
+    function updateValue(key, value) {
+        GM_setValue(key, value)
+        window.location.reload()
     }
 
+    GM_registerMenuCommand(`[${CONFIG.translateTo}] Target Language`, () => {
+        const newLang = prompt("Enter target language code (e.g., en, es, fr, ja):", CONFIG.translateTo);
+        if (newLang !== null) updateValue("translateTo", newLang.trim().toLowerCase() || "en");
+    });
+
+    GM_registerMenuCommand(`[${CONFIG.createMissingTranslateBtn ? "✅" : "❌"}] Create missing translate button for tweets`, () => {
+        updateValue("createMissingTranslateBtn", !CONFIG.createMissingTranslateBtn);
+    });
+
+    GM_registerMenuCommand(`[${CONFIG.debug ? "✅" : "❌"}] Debug Logs`, () => {
+        updateValue("debug", !CONFIG.debug);
+    });
+
+    // debug //
     function debugLog(...args) {
         if (CONFIG.debug) console.log("[GrokToGoogle]", ...args);
     }
 
+    // regex //
+    const emojiRegex = /(\p{Regional_Indicator}{2}|(?:(?:\p{Emoji_Presentation}|\p{Emoji}\uFE0F)(?:\p{Emoji_Modifier}|[\u200D\uFE0F]+\p{Emoji})*))/gu;
+    const emojiRegexp = new RegExp(`^${emojiRegex.source}$`, "u");
+
     // text handler //
+    function isTranslatable(text) {
+        if (!text || text.trim().length === 0) return false;
+
+        const cleanText = text
+            .replace(/https?:\/\/\S+/g, "")
+            .replace(/#\w+/g, "").replace(/@\w+/g, "")
+            .replace(emojiRegex, "")
+            .replace(/[\s\p{Punctuation}\p{Symbol}]+/gu, "")
+            .trim();
+        return cleanText.length > 0;
+    }
+
     function extractTweetText(element) {
         if (!element) return "";
 
@@ -45,9 +85,6 @@
     }
 
     function tweetTextToHtml(text) {
-        const emojiRegex = /(\p{Regional_Indicator}{2}|(?:(?:\p{Emoji_Presentation}|\p{Emoji}\uFE0F)(?:\p{Emoji_Modifier}|[\u200D\uFE0F]+\p{Emoji})*))/gu;
-        const emojiRegexp = new RegExp(`^${emojiRegex.source}$`, "u");
-        
         const spanClass = "tweet-translated-text-span";
         const imgClass = "tweet-translated-emoji-img";
         
@@ -85,9 +122,7 @@
         });
     }
 
-    // css handler //
-    const googleTranslateSvg = `<svg class="tweet-translated-logo" version=1.1 viewBox="0 0 998.1 998.3"x=0 xml:space=preserve xmlns=http://www.w3.org/2000/svg y=0><path d="M931.7 998.3c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4H283.6l260.1 797.9h388z"fill=#DBDBDB /><path d="M931.7 230.4c9.7 0 18.9 3.8 25.8 10.6 6.8 6.7 10.6 15.5 10.6 24.8v667.1c0 9.3-3.7 18.1-10.6 24.8-6.9 6.8-16.1 10.6-25.8 10.6H565.5L324.9 230.4h606.8m0-30H283.6l260.1 797.9h388c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4z"fill=#DCDCDC /><polygon fill=#4352B8 points="482.3,809.8 543.7,998.3 714.4,809.8"/><path d="M936.1 476.1V437H747.6v-63.2h-61.2V437H566.1v39.1h239.4c-12.8 45.1-41.1 87.7-68.7 120.8-48.9-57.9-49.1-76.7-49.1-76.7h-50.8s2.1 28.2 70.7 108.6c-22.3 22.8-39.2 36.3-39.2 36.3l15.6 48.8s23.6-20.3 53.1-51.6c29.6 32.1 67.8 70.7 117.2 116.7l32.1-32.1c-52.9-48-91.7-86.1-120.2-116.7 38.2-45.2 77-102.1 85.2-154.2H936v.1z"fill=#607988 /><path d="M66.4 0C29.9 0 0 29.9 0 66.5v677c0 36.5 29.9 66.4 66.4 66.4h648.1L454.4 0h-388z"fill=#4285F4 /><linearGradient gradientUnits=userSpaceOnUse id=a x1=534.3 x2=998.1 y1=433.2 y2=433.2><stop offset=0 stop-color=#fff stop-opacity=.2 /><stop offset=1 stop-color=#fff stop-opacity=.02 /></linearGradient><path d="M534.3 200.4h397.4c36.5 0 66.4 29.4 66.4 65.4V666L534.3 200.4z"fill=url(#a) /><path d="M371.4 430.6c-2.5 30.3-28.4 75.2-91.1 75.2-54.3 0-98.3-44.9-98.3-100.2s44-100.2 98.3-100.2c30.9 0 51.5 13.4 63.3 24.3l41.2-39.6c-27.1-25-62.4-40.6-104.5-40.6-86.1 0-156 69.9-156 156s69.9 156 156 156c90.2 0 149.8-63.3 149.8-152.6 0-12.8-1.6-22.2-3.7-31.8h-146v53.4l91 .1z"fill=#EEEEEE /><radialGradient cx=65.208 cy=19.366 gradientUnits=userSpaceOnUse id=b r=1398.271><stop offset=0 stop-color=#fff stop-opacity=.1 /><stop offset=1 stop-color=#fff stop-opacity=0 /></radialGradient><path d="M931.7 200.4H518.8L454.4 0h-388C29.9 0 0 29.9 0 66.5v677c0 36.5 29.9 66.4 66.4 66.4h415.9l61.4 188.4h388c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4z"fill=url(#b) /></svg>` /* https://upload.wikimedia.org/wikipedia/commons/d/d7/Google_Translate_logo.svg */;
-    
+    // styling //
     GM_addStyle(`
         .tweet-translated-text-span {
             word-wrap: break-word;
@@ -119,20 +154,79 @@
 
         .tweet-translated-logo {
             vertical-align: top;
-
             -moz-user-select: none;
             -webkit-user-select: none;
             user-select: none;
-
             max-width: 100%;
             position: relative;
             height: 1.25em;
             display: inline-block;
         }
+
+        /* translate button */
+        .tweet-btn-container {
+            display: flex;
+            align-items: center;
+            gap: 4px;
+            padding-top: 4px;
+            min-height: 24px;
+            color: rgb(113, 118, 123);
+            font-family: TwitterChirp, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif;
+            font-size: 13px;
+            line-height: 16px;
+            font-weight: 400;
+        }
+
+        .tweet-btn-button {
+            cursor: pointer;
+            background-color: rgba(0, 0, 0, 0);
+            border: 1px solid rgba(0, 0, 0, 0);
+            border-radius: 9999px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 4px;
+            min-height: 22px;
+            transition: background-color 0.2s, box-shadow 0.2s;
+            outline: none;
+            user-select: none;
+        }
+
+        .tweet-btn-button:hover {
+            background-color: rgba(29, 155, 240, 0.1);
+        }
+
+        .tweet-btn-text-outer {
+            color: rgb(29, 155, 240);
+            display: inline-flex;
+            align-items: center;
+        }
+
+        .tweet-btn-text-inner {
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+            display: inline-flex;
+        }
     `);
 
+    // elements //
+    const googleTranslateSvg = `<svg class="tweet-translated-logo" version=1.1 viewBox="0 0 998.1 998.3"x=0 xml:space=preserve xmlns=http://www.w3.org/2000/svg y=0><path d="M931.7 998.3c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4H283.6l260.1 797.9h388z"fill=#DBDBDB /><path d="M931.7 230.4c9.7 0 18.9 3.8 25.8 10.6 6.8 6.7 10.6 15.5 10.6 24.8v667.1c0 9.3-3.7 18.1-10.6 24.8-6.9 6.8-16.1 10.6-25.8 10.6H565.5L324.9 230.4h606.8m0-30H283.6l260.1 797.9h388c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4z"fill=#DCDCDC /><polygon fill=#4352B8 points="482.3,809.8 543.7,998.3 714.4,809.8"/><path d="M936.1 476.1V437H747.6v-63.2h-61.2V437H566.1v39.1h239.4c-12.8 45.1-41.1 87.7-68.7 120.8-48.9-57.9-49.1-76.7-49.1-76.7h-50.8s2.1 28.2 70.7 108.6c-22.3 22.8-39.2 36.3-39.2 36.3l15.6 48.8s23.6-20.3 53.1-51.6c29.6 32.1 67.8 70.7 117.2 116.7l32.1-32.1c-52.9-48-91.7-86.1-120.2-116.7 38.2-45.2 77-102.1 85.2-154.2H936v.1z"fill=#607988 /><path d="M66.4 0C29.9 0 0 29.9 0 66.5v677c0 36.5 29.9 66.4 66.4 66.4h648.1L454.4 0h-388z"fill=#4285F4 /><linearGradient gradientUnits=userSpaceOnUse id=a x1=534.3 x2=998.1 y1=433.2 y2=433.2><stop offset=0 stop-color=#fff stop-opacity=.2 /><stop offset=1 stop-color=#fff stop-opacity=.02 /></linearGradient><path d="M534.3 200.4h397.4c36.5 0 66.4 29.4 66.4 65.4V666L534.3 200.4z"fill=url(#a) /><path d="M371.4 430.6c-2.5 30.3-28.4 75.2-91.1 75.2-54.3 0-98.3-44.9-98.3-100.2s44-100.2 98.3-100.2c30.9 0 51.5 13.4 63.3 24.3l41.2-39.6c-27.1-25-62.4-40.6-104.5-40.6-86.1 0-156 69.9-156 156s69.9 156 156 156c90.2 0 149.8-63.3 149.8-152.6 0-12.8-1.6-22.2-3.7-31.8h-146v53.4l91 .1z"fill=#EEEEEE /><radialGradient cx=65.208 cy=19.366 gradientUnits=userSpaceOnUse id=b r=1398.271><stop offset=0 stop-color=#fff stop-opacity=.1 /><stop offset=1 stop-color=#fff stop-opacity=0 /></radialGradient><path d="M931.7 200.4H518.8L454.4 0h-388C29.9 0 0 29.9 0 66.5v677c0 36.5 29.9 66.4 66.4 66.4h415.9l61.4 188.4h388c36.5 0 66.4-29.4 66.4-65.4V265.8c0-36-29.9-65.4-66.4-65.4z"fill=url(#b) /></svg>` /* https://upload.wikimedia.org/wikipedia/commons/d/d7/Google_Translate_logo.svg */;
+    const translateButtonDiv = `
+        <div class="tweet-btn-container">
+            ${googleTranslateSvg}
+            <button aria-label="Show translation" role="button" class="tweet-btn-button" type="button">
+                <span class="tweet-btn-text-outer">
+                    <span class="tweet-btn-text-inner">
+                        <span>Show translation</span>
+                    </span>
+                </span>
+            </button>
+        </div>
+    `;
+
     // button handler //
-    const observer = new MutationObserver(() => {
+    const buttonObserver = new MutationObserver(() => {
         document.querySelectorAll('button[aria-label="Show translation"]').forEach((btn) => {
             if (btn.dataset.translateProcessed) return;
             btn.dataset.translateProcessed = "true";
@@ -148,7 +242,7 @@
                 event.stopPropagation();
                 debugLog("Translation button clicked, handling...");
 
-                const tweetContainer = btn.closest("article");
+                const tweetContainer = btn.parentElement.parentElement; // btn.closest("article");
                 const textElement = tweetContainer ? tweetContainer.querySelector('div[data-testid="tweetText"]') : null;
                 if (!textElement) return;
 
@@ -188,7 +282,7 @@
                 debugLog("Starting translation for:", originalText);
 
                 try {
-                    const translatedText = await translateText(originalText, CONFIG.lang_to);
+                    const translatedText = await translateText(originalText, CONFIG.translateTo);
                     const translatedHtml = tweetTextToHtml(translatedText);
                     debugLog("Translation (Text):", translatedText);
                     debugLog("Translation (HTML):", translatedHtml);
@@ -212,5 +306,30 @@
         });
     });
 
-    observer.observe(document.body, { childList: true, subtree: true });
+    buttonObserver.observe(document.body, { childList: true, subtree: true });
+
+    // tweet handler //
+    if (CONFIG.createMissingTranslateBtn) {
+        const tweetObserver = new MutationObserver(() => {
+            document.querySelectorAll('div[data-testid="tweetText"][lang]:not([lang^="' + CONFIG.translateTo + '"])').forEach((tweet) => {
+                if (tweet.dataset.translateBtnProcessed) return;
+                tweet.dataset.translateBtnProcessed = "true";
+
+                const container = tweet.parentElement;
+                if (!container) return;
+
+                const hasCustom = container.querySelector(".tweet-btn-container");
+                const hasNative = container.querySelector('button[aria-label="Show translation"]');
+                if (hasCustom || hasNative) return;
+    
+                const text = extractTweetText(tweet);
+                if (!isTranslatable(text)) return;
+
+                debugLog("Adding translation button to tweet...");
+                container.insertAdjacentHTML("afterbegin", translateButtonDiv);
+            });
+        });
+
+        tweetObserver.observe(document.body, { childList: true, subtree: true });
+    }
 })();
